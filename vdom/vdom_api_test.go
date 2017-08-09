e11adeaa83d4a642cfe359a44996baeafcb958a4
muji <freeformsystems@gmail.com>
2017-08-09 09:20:11 +0800

    Move test code.

---
+++ vdom/vdom_api_test.go
@@ -0,0 +1,72 @@
+package vdom
+
+import (
+  "log"
+  "testing"
+  "io/ioutil"
+  //"golang.org/x/net/html"
+)
+
+// Tests the ability to use the the basic DOM API wrapper 
+// functions.
+func TestVdom(t *testing.T) {
+  file, err := ioutil.ReadFile("../test/fixtures/vdom.html")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  dom := &Vdom{}
+  err = dom.Parse(file)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  if dom == nil {
+    t.Errorf("Expected vdom, got nil")
+  }
+
+  head := dom.Document.FirstChild.NextSibling.FirstChild
+  body := head.NextSibling.NextSibling
+
+  l1 := dom.CreateElement("link")
+  err = dom.AppendChild(head, l1)
+  if err != nil {
+    t.Error(err)
+  }
+
+  foo := dom.CreateElement("foo")
+  err = dom.AppendChild(l1, foo)
+  if err != nil {
+    t.Error(err)
+  }
+
+  l2 := dom.CreateElement("link")
+  err = dom.AppendChild(head, l2)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dom.RemoveChild(head, l1)
+
+  meta := dom.CreateElement("meta")
+  dom.InsertBefore(head, meta, l2)
+
+  script := dom.CreateElement("script")
+  err = dom.AppendChild(body, script)
+  if err != nil {
+    t.Error(err)
+  }
+
+  div := dom.CreateElement("div")
+  err = dom.InsertBefore(body, div, script)
+  if err != nil {
+    t.Error(err)
+  }
+
+  /*
+  err = html.Render(os.Stdout, dom.Document)
+  if err != nil {
+    t.Error(err)
+  }
+  */
+}
--- vdom/vdom_test.go
+++ vdom/vdom_test.go
@@ -9,69 +9,6 @@ import (
   "golang.org/x/net/html"
 )
 
-func TestVdom(t *testing.T) {
-  file, err := ioutil.ReadFile("../test/fixtures/vdom.html")
-  if err != nil {
-    log.Fatal(err)
-  }
-  //log.Println(string(file))
-
-  dom := &Vdom{}
-  err = dom.Parse(file)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  if dom == nil {
-    t.Errorf("Expected vdom, got nil")
-  }
-
-  head := dom.Document.FirstChild.NextSibling.FirstChild
-  body := head.NextSibling.NextSibling
-
-  l1 := dom.CreateElement("link")
-  err = dom.AppendChild(head, l1)
-  if err != nil {
-    t.Error(err)
-  }
-
-  foo := dom.CreateElement("foo")
-  err = dom.AppendChild(l1, foo)
-  if err != nil {
-    t.Error(err)
-  }
-
-  l2 := dom.CreateElement("link")
-  err = dom.AppendChild(head, l2)
-  if err != nil {
-    t.Error(err)
-  }
-
-  dom.RemoveChild(head, l1)
-
-  meta := dom.CreateElement("meta")
-  dom.InsertBefore(head, meta, l2)
-
-  script := dom.CreateElement("script")
-  err = dom.AppendChild(body, script)
-  if err != nil {
-    t.Error(err)
-  }
-
-  div := dom.CreateElement("div")
-  err = dom.InsertBefore(body, div, script)
-  if err != nil {
-    t.Error(err)
-  }
-
-  /*
-  err = html.Render(os.Stdout, dom.Document)
-  if err != nil {
-    t.Error(err)
-  }
-  */
-}
-
 func TestDiff(t *testing.T) {
 
   var diff *Diff
