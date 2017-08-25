93dcd18b72cbe34cffc27b5343f6ea5d995c5df3
muji <freeformsystems@gmail.com>
2017-08-25 18:32:28 +0800

    Improve home page copy.

--- data/app/editor/app.js
+++ data/app/editor/app.js
@@ -1374,7 +1374,12 @@ class EditorApplication {
                   <div class="scroll center">
                     <section class="document">
                       <h1><span>Ꝏ</span>&nbsp;Pageloop</h1>
-                      <p>Collaborative web based document manager designed for rapid creation, editing and publishing of static websites.</p>
+                      <p class="leader">Collaborative realtime web based document manager</p>
+                      <p>Designed for static websites pageloop enables you to create modern websites from lovingly crafted responsive templates in a single click.</p>
+                      <p>To get started go to the <a href="#apps" title="Application manager">application manager</a> and choose from one of our beautiful, responsive templates for your next website.</p>
+                      <p>You can use the application editor to customize your website using an intuitive drag and drop interface. Technical folks can edit the source code directly and you can export your files at any time if you need to.</p>
+                      <p>For those working as a team you can assign semantic roles (such as designer, editor, translator etc) so that we can customize the interface to best fit your needs.</p>
+                      <p>We recommend you read the <a href="#docs" title="Documentation & help">documentation</a> to learn more about how pageloop works.</p>
                     </section>
                   </div>
                 </div>
