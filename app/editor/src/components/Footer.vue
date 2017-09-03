ffc5dda3162c33e2ac8cca875d3832ab82a9ca2e
muji <freeformsystems@gmail.com>
2017-09-03 13:25:55 +0800

    Migrate routes.

--- app/editor/src/components/Footer.vue
+++ app/editor/src/components/Footer.vue
@@ -1,9 +1,29 @@
 <template>
+  <footer>
+    <p class="log" v-bind:class="{error: error}">{{prefix}}{{message}}</p>
+  </footer>
 </template>
 
 <script>
 export default {
-  name: 'app-footer'
+  name: 'app-footer',
+  computed: {
+    message: function () {
+      return this.$store.state.log.toString()
+    },
+    error: function () {
+      return (this.$store.state.log.last instanceof Error)
+    },
+    prefix: function () {
+      let msg = this.$store.state.log.last
+      if (msg && this.error) {
+        return '! '
+      } else if (this.message && !this.error) {
+        return '# '
+      }
+      return ''
+    }
+  }
 }
 </script>
 
