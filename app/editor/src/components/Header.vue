23e8baa1c3ce9b983929d83b1906eb3a0f4ddc5f
muji <freeformsystems@gmail.com>
2017-09-03 13:18:49 +0800

    Add fonts and favicon.

--- app/editor/src/components/Header.vue
+++ app/editor/src/components/Header.vue
@@ -1,9 +1,44 @@
 <template>
+  <header class="clearfix">
+    <nav class="home">
+      <a
+        @click="$store.dispatch('navigate', {href: 'edit'})"
+        :class="{selected: selectedView === 'edit', hidden: !this.$store.state.hasApplication()}"
+        title="Edit Current Application">{{name}}</a>
+    </nav>
+    <nav class="main">
+      <a
+        @click="$store.dispatch('navigate', {href: 'home'})"
+        class="home"
+        :class="{selected: selectedView === 'home'}"
+        title="Home page">Ꝏ </a>
+      <a
+        @click="$store.dispatch('navigate', {href: 'apps'})"
+        :class="{selected: selectedView === 'apps'}"
+        title="View and edit applications">Apps</a>
+      <a
+        @click="$store.dispatch('navigate', {href: 'docs'})"
+        :class="{selected: selectedView === 'docs'}"
+        title="Documentation">Docs</a>
+      <a
+        @click="$store.dispatch('navigate', {href: 'settings'})"
+        :class="{selected: selectedView === 'settings'}"
+        title="Settings">Settings</a>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'app-header'
+  name: 'app-header',
+  computed: {
+    name: function () {
+      return this.$store.state.application
+    },
+    selectedView: function () {
+      return this.$store.state.mainView
+    }
+  }
 }
 </script>
 
