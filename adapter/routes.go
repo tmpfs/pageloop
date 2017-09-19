bf351f97068913f723111e3f25d8a7ff1e86c542
muji <freeformsystems@gmail.com>
2017-09-19 12:31:38 +0800

    Fix comment issue.

--- adapter/routes.go
+++ adapter/routes.go
@@ -94,14 +94,12 @@ func init() {
   push("File.Read", NewAction(OperationRead, "/apps/*/*/files/*"),
     &CommandDefinition{MethodName: "ReadFile", Status: http.StatusOK, Arguments: fileArg})
 
-  /*
   // GET /apps/{container}/{application}/src/{url}
-  push("File.ReadSource", NewAction(OperationRead, "/apps/*/*/src/*"),
-    &CommandDefinition{MethodName: "ReadFileSource", Status: http.StatusOK, Arguments: fileArg})
+  //push("File.ReadSource", NewAction(OperationRead, "/apps/*/*/src/*"),
+    //&CommandDefinition{MethodName: "ReadFileSource", Status: http.StatusOK, Arguments: fileArg})
   // GET /apps/{container}/{application}/raw/{url}
-  push("File.ReadSourceRaw", NewAction(OperationRead, "/apps/*/*/raw/*"),
-    &CommandDefinition{MethodName: "ReadFileSourceRaw", Status: http.StatusOK, Arguments: fileArg})
-  */
+  //push("File.ReadSourceRaw", NewAction(OperationRead, "/apps/*/*/raw/*"),
+    //&CommandDefinition{MethodName: "ReadFileSourceRaw", Status: http.StatusOK, Arguments: fileArg})
 
   // PUT /apps/{container}/{application}/files/{url}
   push("File.Create", NewAction(OperationCreate, "/apps/*/*/files/*"),
