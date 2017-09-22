fa2e0f3c71ef78ffcd7e4e5685b9d468e6c462b9
muji <freeformsystems@gmail.com>
2017-09-22 19:28:20 +0800

    Render statistics in settings -> about.

--- app/system/editor/source/src/components/settings/AboutSettings.vue
+++ app/system/editor/source/src/components/settings/AboutSettings.vue
@@ -1,8 +1,21 @@
 <template>
-  <p class="small">
-    <span>{{meta.info.name}} v{{meta.info.version}}</span>
-    {{meta.stats}}
-  </p>
+  <div class="small">
+    <div class="info">
+      <span class="meta">{{meta.info.name}} v{{meta.info.version}}</span>
+      <span class="uptime">{{meta.stats.uptime.value}}</span>
+    </div>
+    <div class="stats">
+      <div v-for="group, key in meta.stats">
+        <h4>{{key}}</h4>
+        <ul class="stat">
+          <li v-for="v, k in group">
+            <span>{{k}}</span>
+            <span>{{v}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,4 +33,48 @@ export default {
 </script>
 
 <style scoped>
+
+  .info {
+    clear: both;
+    height: 4.2rem;
+    margin-bottom: 1rem;
+    padding: 1rem 0;
+    border-bottom: 1px solid var(--border-color);
+  }
+
+  .meta, .uptime {
+    display: inline-block;
+  }
+
+  .meta {
+    float: left;
+  }
+
+  .uptime {
+    float: right;
+  }
+
+  .stats h4 {
+    text-transform: uppercase;
+    background: var(--base03-color);
+    padding-left: 0.5rem;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    display: table;
+    width: 100%;
+  }
+
+  li {
+    display: table-row;
+    width: 100%;
+  }
+
+  li > span {
+    display: table-cell;
+    width: 50%;
+  }
 </style>
