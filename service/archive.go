e993b450572e81d40749cebc37d6675e1e425f17
muji <freeformsystems@gmail.com>
2017-09-26 10:27:06 +0800

    Improve export archive logic.

--- service/archive.go
+++ service/archive.go
@@ -37,60 +37,75 @@ type ArchiveRequest struct {
 
 // Export a zip archive of application files.
 func (s *ArchiveService) Export(archive *ArchiveRequest, reply *ServiceReply) *StatusError {
-  if _, a, err := LookupApplication(s.Host, archive.Application); err != nil {
-    return err
-  } else {
 
-    //fmt.Printf("%#v\n", archive.Type)
+  // Add source files to the archive
+  source := func (z *zip.Writer, app *Application, prefix string) *StatusError {
+    for _, file := range app.Files {
+      url := prefix + file.Url
+      f, err := z.Create(url)
+      if err != nil {
+        return CommandError(http.StatusInternalServerError, err.Error())
+      }
 
-    z := zip.NewWriter(archive.Writer)
-    // Send using in-memory file data
-    if archive.Type == ArchiveSource {
-      for _, file := range a.Files {
-        f, err := z.Create(file.Url)
-        if err != nil {
-          return CommandError(http.StatusInternalServerError, err.Error())
-        }
+      // Send using in-memory file data
+      _, err = f.Write([]byte(file.Source(true)))
+      if err != nil {
+        return CommandError(http.StatusInternalServerError, err.Error())
+      }
+    }
+    return nil
+  }
 
-        _, err = f.Write([]byte(file.Source(true)))
+  // Add public files to the archive
+  public := func (z *zip.Writer, app *Application, prefix string) *StatusError {
+    // Walk the public files.
+    dir := app.PublicDirectory()
+    err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+      if err != nil {
+        return err
+      }
+
+      if path == dir || info.IsDir() {
+        return nil
+      }
+
+      // Assuming POSIX style fs
+      url := strings.TrimPrefix(path, dir)
+
+      f, err := z.Create(url)
+      if err != nil {
+        return CommandError(http.StatusInternalServerError, err.Error())
+      }
+
+      // TODO: stream content from disc
+      if content, err := ioutil.ReadFile(path); err != nil {
+        return err
+      } else {
+        _, err = f.Write(content)
         if err != nil {
           return CommandError(http.StatusInternalServerError, err.Error())
         }
       }
-    // Walk the public files.
+
+      return nil
+    })
+    if err != nil {
+      return CommandError(http.StatusInternalServerError, err.Error())
+    }
+    return nil
+  }
+
+  if _, a, err := LookupApplication(s.Host, archive.Application); err != nil {
+    return err
+  } else {
+    z := zip.NewWriter(archive.Writer)
+    if archive.Type == ArchiveSource {
+      if err := source(z, a, ""); err != nil {
+        return err
+      }
     } else {
-      dir := a.PublicDirectory()
-      err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-          return err
-        }
-
-        if path == dir || info.IsDir() {
-          return nil
-        }
-
-        // Assuming POSIX style fs
-        url := strings.TrimPrefix(path, dir)
-
-        f, err := z.Create(url)
-        if err != nil {
-          return CommandError(http.StatusInternalServerError, err.Error())
-        }
-
-        // TODO: stream content from disc
-        if content, err := ioutil.ReadFile(path); err != nil {
-          return err
-        } else {
-          _, err = f.Write(content)
-          if err != nil {
-            return CommandError(http.StatusInternalServerError, err.Error())
-          }
-        }
-
-        return nil
-      })
-      if err != nil {
-        return CommandError(http.StatusInternalServerError, err.Error())
+      if err := public(z, a, ""); err != nil {
+        return err
       }
     }
 
